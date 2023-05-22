14d20059b7458a26228657b9fd493372ffefe401
Andrey4ik222 <107884250+Andrey4ik222@users.noreply.github.com>
2023-05-22 22:11:53 +0200

    refactoring code

--- src/components/Form/Form.js
+++ src/components/Form/Form.js
@@ -12,91 +12,77 @@ const Basic = () => {
     setSubmitting(false);
   };
 
+  const validate = (values) => {
+    const errors = {};
+    const emailRegex = /^[A-Z0-9._%+-]+@[A-Z0-9.-]+\.[A-Z]{2,}$/i;
+    const phoneRegex = /^[0-9]+$/;
+
+    if (!values.firstName) {
+      errors.firstName = "Name cannot be empty!";
+    }
+
+    if (!values.email) {
+      errors.email = "Email cannot be empty!";
+    } else if (!emailRegex.test(values.email)) {
+      errors.email = "Invalid email address!";
+    }
+
+    if (!values.phone) {
+      errors.phone = "Phone cannot be empty!";
+    } else if (!phoneRegex.test(values.phone)) {
+      errors.phone = "Only numbers allowed!";
+    } else if (values.phone.length !== 12) {
+      errors.phone = "Must be 12 digits!";
+    }
+
+    return errors;
+  };
+
+  const inputFields = [
+    { name: "firstName", type: "text", placeholder: "name..." },
+    { name: "email", type: "email", placeholder: "email..." },
+    { name: "phone", type: "text", placeholder: "phone..." },
+  ];
+
+  const renderForm = ({
+    values,
+    errors,
+    touched,
+    handleChange,
+    handleBlur,
+    handleSubmit,
+    isSubmitting,
+  }) => (
+    <form onSubmit={handleSubmit} className="form">
+      {inputFields.map((field) => (
+        <div key={field.name}>
+          <input
+            type={field.type}
+            name={field.name}
+            placeholder={field.placeholder}
+            onChange={handleChange}
+            onBlur={handleBlur}
+            value={values[field.name]}
+          />
+          <p className="validation-info">
+            {errors[field.name] && touched[field.name] && errors[field.name]}
+          </p>
+        </div>
+      ))}
+      <button type="submit" disabled={isSubmitting} className="btn-submit">
+        Submit
+      </button>
+    </form>
+  );
+
   return (
     <div className="form-wrapper">
       <Formik
         initialValues={{ firstName: "", email: "", phone: "" }}
-        validate={(values) => {
-          const errors = {};
-          const emailRegex = /^[A-Z0-9._%+-]+@[A-Z0-9.-]+\.[A-Z]{2,}$/i;
-          const phoneRegex = /^[0-9]+$/;
-
-          if (!values.firstName) {
-            errors.firstName = "Name cannot be empty!";
-          }
-
-          if (!values.email) {
-            errors.email = "Email cannot be empty!";
-          } else if (!emailRegex.test(values.email)) {
-            errors.email = "Invalid email address!";
-          }
-
-          if (!values.phone) {
-            errors.phone = "Phone cannot be empty!";
-          } else if (!phoneRegex.test(values.phone)) {
-            errors.phone = "Only numbers allowed!";
-          } else if (values.phone.length !== 12) {
-            errors.phone = "Must be 12 digits!";
-          }
-
-          return errors;
-        }}
+        validate={validate}
         onSubmit={handleSubmit}
       >
-        {({
-          values,
-          errors,
-          touched,
-          handleChange,
-          handleBlur,
-          handleSubmit,
-          isSubmitting,
-        }) => (
-          <form onSubmit={handleSubmit} className="form">
-            <input
-              type="text"
-              name="firstName"
-              placeholder="name..."
-              onChange={handleChange}
-              onBlur={handleBlur}
-              value={values.firstName}
-            />
-            <p className="validation-info">
-              {errors.firstName && touched.firstName && errors.firstName}
-            </p>
-
-            <input
-              type="email"
-              name="email"
-              placeholder="email..."
-              onChange={handleChange}
-              onBlur={handleBlur}
-              value={values.email}
-            />
-            <p className="validation-info">
-              {errors.email && touched.email && errors.email}
-            </p>
-
-            <input
-              type="text"
-              name="phone"
-              placeholder="phone..."
-              onChange={handleChange}
-              onBlur={handleBlur}
-              value={values.phone}
-            />
-            <p className="validation-info">
-              {errors.phone && touched.phone && errors.phone}
-            </p>
-            <button
-              type="submit"
-              disabled={isSubmitting}
-              className="btn-submit"
-            >
-              Submit
-            </button>
-          </form>
-        )}
+        {renderForm}
       </Formik>
 
       {submittedValues && (
